da817287e7c77bc3297cbaa04e2c39ef20de199c
Xtrendence <xtrendence@yahoo.com>
2020-07-23 11:04:34 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -137,7 +137,6 @@ document.addEventListener("DOMContentLoaded", function() {
 	}
 });
 
-// Replace all occurrences in a string.
 String.prototype.replaceAll = function(str1, str2, ignore) {
 	return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
 }
